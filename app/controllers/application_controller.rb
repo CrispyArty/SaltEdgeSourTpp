9649a7ec57fd015a4baaecc28a461a72166848fc
Artem <artemti0@gmail.com>
2026-04-07 15:31:32 +0300

    feat: show error page if app was not setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
   include AuthenticationConcern
+  before_action :verify_certificate!
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  private
+
+  def verify_certificate!
+    render 'errors/not_setup', layout: 'auth', status: 500 if Rails.root.join("storage", "certificates", "client_signed_certificate.crt")
+  end
 end
