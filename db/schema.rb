3c398778aa0bdacfc576fc72d48cf120c559375c
Artiom Teleshman <artiom.teleshman@saltedge.com>
2026-07-03 15:46:55 +0300

    feat: new schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_22_072905) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_12_125859) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -20,7 +20,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_22_072905) do
     t.string "error_class"
     t.text "error_message"
     t.string "method", null: false
-    t.boolean "pending", default: true, null: false
+    t.boolean "pending", default: false
     t.jsonb "request_body"
     t.jsonb "request_headers"
     t.text "response_body"
